d5764e711999c064fcb319dd703cb1cb3cb4cd1e
Julien Sié <julien.sie@gmail.com>
2020-06-20 19:48:21 +0200

    set password size with args

--- main.go
+++ main.go
@@ -2,11 +2,29 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 
 	"./generator"
 )
 
+var size = 16
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 func main() {
-	pwd := generator.Generate(16)
+
+	if len(os.Args) >= 2 {
+		s, err := strconv.Atoi(os.Args[1])
+		check(err)
+		size = s
+	}
+
+	fmt.Printf("** PASSWORD GENERATOR**\npassword length : \033[1;33m%v\033[0m\n", size)
+	pwd := generator.Generate(size)
 	fmt.Printf("code : \033[1;32m%s\033[0m\n", pwd)
 }
