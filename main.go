90320a853da9a07fe96dab34172abc09bb7ea7b6
Julien Sié <julien.sie@gmail.com>
2020-06-20 19:56:59 +0200

    block size < 8 and use default value instead

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 
 var size = 16
 
+const minSize = 8
+
 func check(e error) {
 	if e != nil {
 		panic(e)
@@ -20,8 +22,15 @@ func main() {
 
 	if len(os.Args) >= 2 {
 		s, err := strconv.Atoi(os.Args[1])
+
 		check(err)
-		size = s
+
+		switch {
+		case s <= minSize:
+			fmt.Printf("\033[1;31mprovided size is less than %v, changed to %v !!!\033[0m\n", minSize, size)
+		default:
+			size = s
+		}
 	}
 
 	fmt.Printf("** PASSWORD GENERATOR**\npassword length : \033[1;33m%v\033[0m\n", size)
