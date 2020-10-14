0b4cd6d894396b4e7048776c59089affc187a00c
Julien Sié <julien.sie@gmail.com>
2020-10-14 14:08:14 +0200

    module refactoring

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-
-	"github.com/trendev/pwdgen/generator"
 )
 
 func main() {
@@ -17,13 +15,12 @@ func main() {
 
 func generate(length int) {
 	l := length
-	min := generator.MinLength
-	if l < min {
-		fmt.Printf("\033[1;31mprovided length is less than %v, changed to %v !!!\033[0m\n", l, min)
-		l = min
+	if l < MinLength {
+		fmt.Printf("\033[1;31mprovided length is less than %v, changed to %v !!!\033[0m\n", l, MinLength)
+		l = MinLength
 	}
 
 	fmt.Printf("Password length : \033[1;33m%v\033[0m\n", l)
-	pwd := generator.Generate(l)
+	pwd := Generate(l)
 	fmt.Printf("Code : \033[1;32m%s\033[0m\n", pwd)
 }
