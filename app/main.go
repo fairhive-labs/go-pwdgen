df7b90f9662ed1bec06d0993db61e5b5f876389c
juliensie <julien.sie@gmail.com>
2022-02-20 21:23:30 +0100

    refactor with generate handler

--- app/main.go
+++ app/main.go
@@ -2,23 +2,24 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/trendev/go-pwdgen/generator"
 )
 
+func generate(c *gin.Context) {
+	l := 16
+	pwd := generator.Generate(l)
+	c.JSON(http.StatusOK, gin.H{
+		"length":   l,
+		"password": pwd,
+	})
+}
+
 func main() {
 
 	r := gin.Default()
-
-	r.GET("/", func(c *gin.Context) {
-		l := 16
-		pwd := generator.Generate(l)
-		c.JSON(200, gin.H{
-			"length":   l,
-			"password": pwd,
-		})
-	})
-
+	r.GET("/", generate)
 	log.Fatal(r.Run())
 }
