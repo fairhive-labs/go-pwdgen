f9e1b22dcfb4f2fff2cd7e978829d1698991e545
juliensie <julien.sie@gmail.com>
2022-02-21 00:08:46 +0100

    test with incompatible length

--- app/main_test.go
+++ app/main_test.go
@@ -67,3 +67,30 @@ func TestMainRouteWith32Length(t *testing.T) {
 		t.FailNow()
 	}
 }
+
+func TestMainRouteWithIncompatibleLength(t *testing.T) {
+	router := setupRouter()
+	w := httptest.NewRecorder()
+	length := 16
+	req, _ := http.NewRequest("GET", fmt.Sprintf("/?l=%s", "mistake"), nil)
+	router.ServeHTTP(w, req)
+
+	if w.Code != http.StatusOK {
+		t.Errorf("%d should be %d", w.Code, http.StatusOK)
+		t.FailNow()
+	}
+
+	var r response
+
+	json.NewDecoder(w.Body).Decode(&r)
+
+	if r.Length != length {
+		t.Errorf("length should be %d", length)
+		t.FailNow()
+	}
+
+	if len(r.Password) != length {
+		t.Errorf("%s length should be %d", r.Password, length)
+		t.FailNow()
+	}
+}
