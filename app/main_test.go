3565b0578c87b945475319bb3bd82584384c0188
juliensie <julien.sie@gmail.com>
2022-02-21 00:06:10 +0100

    test w/. 32 length

--- app/main_test.go
+++ app/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -38,4 +39,31 @@ func TestMainRoute(t *testing.T) {
 		t.FailNow()
 	}
 
+}
+
+func TestMainRouteWith32Length(t *testing.T) {
+	router := setupRouter()
+	w := httptest.NewRecorder()
+	length := 32
+	req, _ := http.NewRequest("GET", fmt.Sprintf("/?l=%d", length), nil)
+	router.ServeHTTP(w, req)
+
+	if w.Code != http.StatusOK {
+		t.Errorf("%d should be %d", w.Code, http.StatusOK)
+		t.FailNow()
+	}
+
+	var r response
+
+	json.NewDecoder(w.Body).Decode(&r)
+
+	if r.Length != length {
+		t.Errorf("length should be %d", length)
+		t.FailNow()
+	}
+
+	if len(r.Password) != length {
+		t.Errorf("%s length should be %d", r.Password, length)
+		t.FailNow()
+	}
 }
