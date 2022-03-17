38214f8bc77a1ae6d3bc2c5c6b28d928959aaf4f
_whyvrafvr <jsie@trendev.fr>
2022-03-17 17:20:19 +0100

    format w/. html template

--- app/main.go
+++ app/main.go
@@ -11,7 +11,20 @@ import (
 
 const length = 16
 
-func generate(c *gin.Context) {
+func main() {
+	r := setupRouter()
+	log.Fatal(r.Run())
+}
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+	r.GET("/json", generateJSON)
+	r.GET("/", generateHTML)
+	return r
+}
+
+func generateJSON(c *gin.Context) {
 	l := c.DefaultQuery("l", strconv.Itoa(length))
 
 	ln, err := strconv.Atoi(l)
@@ -28,13 +41,19 @@ func generate(c *gin.Context) {
 	})
 }
 
-func setupRouter() *gin.Engine {
-	r := gin.Default()
-	r.GET("/json", generate)
-	return r
-}
+func generateHTML(c *gin.Context) {
+	l := c.DefaultQuery("l", strconv.Itoa(length))
 
-func main() {
-	r := setupRouter()
-	log.Fatal(r.Run())
+	ln, err := strconv.Atoi(l)
+	if err != nil {
+		log.Println("incorrect length, converted to", length)
+		ln = length
+	}
+
+	pwd := generator.Generate(ln)
+
+	c.HTML(http.StatusOK, "index.html", gin.H{
+		"length":   len(pwd),
+		"password": pwd,
+	})
 }
